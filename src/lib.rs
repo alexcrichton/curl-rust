81cd4b96ffdc7598551eaac0131d7c98728ca96f
Alex Crichton <alex@alexcrichton.com>
2016-03-30 23:57:46 -0700

    Add bindings to version information.

--- src/lib.rs
+++ src/lib.rs
@@ -2,28 +2,41 @@
 
 #![deny(missing_docs)]
 
-// extern crate libc;
-// extern crate url;
-
 extern crate curl_sys;
+extern crate libc;
 
-// #[cfg(all(unix, not(target_os = "macos")))]
-// extern crate openssl_sys as openssl;
-//
-// pub use ffi::easy::ProgressCb;
-// pub use ffi::err::ErrCode;
-//
-// pub use error::Error;
-//
-// // Version accessors
-// pub use ffi::version::{
-//     Version,
-//     version,
-//     version_info,
-//     Protocols
-// };
+use std::ffi::CStr;
+use std::str;
+use std::sync::{Once, ONCE_INIT};
 
 pub use error::{Error, ShareError, MultiError};
 mod error;
-// mod ffi;
-// pub mod http;
+
+pub use version::{Version, Protocols};
+mod version;
+
+/// Initializes the underlying libcurl library.
+///
+/// It's not required to call this before the library is used, but it's
+/// recommended to do so as soon as the program starts.
+pub fn init() {
+    static INIT: Once = ONCE_INIT;
+    INIT.call_once(|| {
+        unsafe {
+            curl_sys::curl_global_init(curl_sys::CURL_GLOBAL_ALL);
+            libc::atexit(cleanup);
+        }
+    });
+
+    extern fn cleanup() {
+        unsafe { curl_sys::curl_global_cleanup(); }
+    }
+}
+
+unsafe fn opt_str<'a>(ptr: *const libc::c_char) -> Option<&'a str> {
+    if ptr.is_null() {
+        None
+    } else {
+        Some(str::from_utf8(CStr::from_ptr(ptr).to_bytes()).unwrap())
+    }
+}

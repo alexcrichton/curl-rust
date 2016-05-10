3293678ba090304b7aa17e1e84d8ca656a81a540
Alex Crichton <alex@alexcrichton.com>
2016-05-10 14:49:10 -0700

    Add some crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,45 @@
-//! TODO: dox
+//! Rust bindings to the libcurl C library
+//!
+//! This crate contains bindings for an HTTP/HTTPS client which is powered by
+//! [libcurl], the same library behind the `curl` command line tool. The API
+//! currently closely matches that of libcurl itself, except that a Rustic layer
+//! of safety is applied on top.
+//!
+//! [libcurl]: https://curl.haxx.se/libcurl/
+//!
+//! # The "Easy" API
+//!
+//! The easiest way to send a request is to use the `Easy` api which corresponds
+//! to `CURL` in libcurl. This handle supports a wide variety of options and can
+//! be used to make a single blocking request in a thread. Callbacks can be
+//! specified to deal with data as it arrives and a handle can be reused to
+//! cache connections and such.
+//!
+//! ```rust,no_run
+//! use curl::easy::Easy;
+//!
+//! // Write the contents of rust-lang.org to stdout
+//! let mut easy = Easy::new();
+//! easy.url("https://www.rust-lang.org/").unwrap();
+//! easy.perform().unwrap();
+//! ```
+//!
+//! # What about multiple concurrent HTTP requests?
+//!
+//! One option you have currently is to send multiple requests in multiple
+//! threads, but otherwise libcurl has a "multi" interface for doing this
+//! operation. This unfortunately has not yet been bound in this crate, but
+//! those should be coming soon!
+//!
+//! # Where does libcurl come from?
+//!
+//! This crate links to the `curl-sys` crate which is in turn responsible for
+//! acquiring and linking to the libcurl library. Currently this crate will
+//! build libcurl from source if one is not already detected on the system.
+//!
+//! There is a large number of releases for libcurl, all with different sets of
+//! capabilities. Robust programs may wish to inspect `Version::get()` to test
+//! what features are implemented in the linked build of libcurl at runtime.
 
 #![deny(missing_docs)]
 

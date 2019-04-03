737339b306b9fc0c43d804f50e1688b26432b418
Hristo Venev <hristo@venev.name>
2019-04-03 13:57:12 +0300

    Only use openssl-probe if we're using OpenSSL.

--- build.rs
+++ build.rs
@@ -2,25 +2,32 @@ use std::env;
 use std::str::FromStr;
 
 fn main() {
+    let use_openssl;
+
     // OpenSSL >= 1.1.0 can be initialized concurrently and is initialized correctly by libcurl.
     // <= 1.0.2 need locking callbacks, which are provided by openssl_sys::init().
     match env::var("DEP_OPENSSL_VERSION") {
         Ok(ver) => {
+            use_openssl = true;
             let ver = u32::from_str(&ver).unwrap();
             if ver < 110 {
                 println!("cargo:rustc-cfg=need_openssl_init");
             }
+
         }
-        Err(env::VarError::NotPresent) => {}
+        Err(env::VarError::NotPresent) => {
+            use_openssl = false;
+        }
         Err(e) => panic!(e),
     }
 
-    // The system libcurl should have the default certificate paths configured.
-    match env::var("DEP_CURL_STATIC") {
-        Ok(_) => {
-            println!("cargo:rustc-cfg=need_openssl_probe");
+    if use_openssl {
+        // The system libcurl should have the default certificate paths configured.
+        match env::var_os("DEP_CURL_STATIC") {
+            Some(_) => {
+                println!("cargo:rustc-cfg=need_openssl_probe");
+            }
+            None => {}
         }
-        Err(env::VarError::NotPresent) => {}
-        Err(e) => panic!(e),
     }
 }

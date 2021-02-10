145814083d0758c8242f18753870fd908918d124
Stephen M. Coakley <me@stephencoakley.com>
2021-02-09 20:31:12 -0600

    Formatting

--- curl-sys/build.rs
+++ curl-sys/build.rs
@@ -261,6 +261,10 @@ fn main() {
         } else {
             cfg.define("HAVE_UNIX", None);
         }
+    } else if cfg!(feature = "rustls") {
+        cfg.define("USE_RUSTLS", None)
+            .file("curl/lib/vtls/rustls.c")
+            .include(env::var_os("DEP_CRUSTLS_INCLUDE").unwrap());
     } else if cfg!(feature = "ssl") {
         if windows {
             // For windows, spnego feature is auto on in case ssl feature is on.

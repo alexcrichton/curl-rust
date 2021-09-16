33dfe3389dd532ac4355d4f34a99a85985d319ce
Alex Crichton <alex@alexcrichton.com>
2021-09-16 13:21:20 -0500

    Build more curl files unconditionally (#406)
    Helps fix targets where `-gc-sections` isn't specified.

--- ci/run.sh
+++ ci/run.sh
@@ -11,8 +11,14 @@ if [ -z "$NO_RUN" ]; then
     cargo test --target $TARGET
     cargo test --target $TARGET --features static-curl
     cargo test --target $TARGET --features static-curl,protocol-ftp
+
+    # Note that `-Clink-dead-code` is passed here to suppress `--gc-sections` to
+    # help confirm that we're compiling everything necessary for curl itself.
+    RUSTFLAGS=-Clink-dead-code \
     cargo run --manifest-path systest/Cargo.toml --target $TARGET
+    RUSTFLAGS=-Clink-dead-code \
     cargo run --manifest-path systest/Cargo.toml --target $TARGET --features curl-sys/static-curl,curl-sys/protocol-ftp
+
     cargo doc --no-deps --target $TARGET
     cargo doc --no-deps -p curl-sys --target $TARGET
 fi

e71a6c064980af79446e203f9160f34992105666
Alex Crichton <alex@alexcrichton.com>
2021-11-01 14:46:51 -0700

    Update OpenSSL initialization check
    As described in sfackler/rust-openssl#1548

--- build.rs
+++ build.rs
@@ -1,13 +1,12 @@
 use std::env;
-use std::str::FromStr;
 
 fn main() {
     // OpenSSL >= 1.1.0 can be initialized concurrently and is initialized correctly by libcurl.
     // <= 1.0.2 need locking callbacks, which are provided by openssl_sys::init().
-    let use_openssl = match env::var("DEP_OPENSSL_VERSION") {
-        Ok(ver) => {
-            let ver = u32::from_str(&ver).unwrap();
-            if ver < 110 {
+    let use_openssl = match env::var("DEP_OPENSSL_VERSION_NUMBER") {
+        Ok(version) => {
+            let version = u64::from_str_radix(&version, 16).unwrap();
+            if version < 0x1_01_00_00_0 {
                 println!("cargo:rustc-cfg=need_openssl_init");
             }
             true

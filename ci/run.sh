5031066527c71a9ca8c2b7ca6d697839f56e2cd3
zengxs <xiangsong.zeng@outlook.com>
2022-11-24 06:52:22 +0000

    install cmake for zlib-ng build

--- ci/run.sh
+++ ci/run.sh
@@ -2,6 +2,30 @@
 
 set -ex
 
+# install cmake. cmake required for zlib-ng build, and old cmake may not work,
+# so we install the latest one. this step only run for ci docker environment to
+# avoid local development environment pollution.
+if [ -f /.dockerenv ]; then
+    CMAKE_URL=https://github.com/Kitware/CMake/releases/download/v3.25.0/cmake-3.25.0-linux-x86_64.tar.gz
+
+    # check curl or wget is installed for downloading cmake
+    if command -v curl >/dev/null 2>&1; then
+        DL="curl -sSL"
+    elif command -v wget >/dev/null 2>&1; then
+        DL="wget -qO-"
+    else
+        echo "curl or wget is required to download cmake"
+        exit 1
+    fi
+
+    if [ ! -f /opt/cmake/bin/cmake ]; then  # check cmake is installed (avoid multiple install)
+        # download cmake and install
+        mkdir -p /opt/cmake
+        $DL $CMAKE_URL | tar -C /opt/cmake -xz --strip-components=1
+        export PATH=/opt/cmake/bin:$PATH
+    fi
+fi
+
 cargo test --target $TARGET --no-run
 # First test with no extra protocols enabled.
 cargo test --target $TARGET --no-run --features static-curl

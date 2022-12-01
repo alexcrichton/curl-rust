ee12368aecbbd664ff44dac30d71b065dd8c1c11
zengxs <xiangsong.zeng@outlook.com>
2022-12-01 06:38:35 +0000

    update

--- tests/zlib.rs
+++ tests/zlib.rs
@@ -3,31 +3,22 @@ fn test_static_zlib() {
     let version = curl::Version::get();
     let feature_libz = version.feature_libz();
 
-    #[cfg(all(
-        feature = "static-curl",
-        not(any(feature = "zlib", feature = "zlib-ng-compat")),
-    ))]
-    assert_eq!(feature_libz, false);
-    #[cfg(all(
-        feature = "static-curl",
-        any(feature = "zlib", feature = "zlib-ng-compat")
-    ))]
+    #[cfg(feature = "static-curl")]
     assert_eq!(feature_libz, true);
+    #[cfg(feature = "static-curl-no-zlib")]
+    assert_eq!(feature_libz, false);
 
-    #[cfg(all(
-        feature = "static-curl",
-        any(feature = "zlib", feature = "zlib-ng-compat")
-    ))]
+    #[cfg(not(feature = "static-curl-no-zlib"))]
     {
         let libz_version = version.libz_version();
         assert!(libz_version.is_some());
 
         let libz_version = libz_version.unwrap();
 
-        #[cfg(all(feature = "zlib", not(feature = "zlib-ng-compat")))]
-        assert_eq!(libz_version.contains("zlib-ng"), false);
-
-        #[cfg(all(not(feature = "zlib"), feature = "zlib-ng-compat"))]
+        #[cfg(feature = "zlib-ng-compat")]
         assert_eq!(libz_version.contains("zlib-ng"), true);
+
+        #[cfg(not(feature = "zlib-ng-compat"))]
+        assert_eq!(libz_version.contains("zlib-ng"), false);
     }
 }

a10b1edf1b5479f04025b52ea974c8cc6e62c93f
zengxs <xiangsong.zeng@outlook.com>
2022-12-01 06:55:05 +0000

    update

--- tests/zlib.rs
+++ tests/zlib.rs
@@ -3,22 +3,22 @@ fn test_static_zlib() {
     let version = curl::Version::get();
     let feature_libz = version.feature_libz();
 
-    #[cfg(feature = "static-curl")]
-    assert_eq!(feature_libz, true);
-    #[cfg(feature = "static-curl-no-zlib")]
-    assert_eq!(feature_libz, false);
+    if cfg!(feature = "static-curl-no-zlib") {
+        assert_eq!(feature_libz, false);
+    }
 
-    #[cfg(not(feature = "static-curl-no-zlib"))]
-    {
+    if cfg!(feature = "static-curl") || cfg!(feature = "zlib-ng-compat") {
+        assert_eq!(feature_libz, true);
+
         let libz_version = version.libz_version();
         assert!(libz_version.is_some());
 
         let libz_version = libz_version.unwrap();
 
-        #[cfg(feature = "zlib-ng-compat")]
-        assert_eq!(libz_version.contains("zlib-ng"), true);
-
-        #[cfg(not(feature = "zlib-ng-compat"))]
-        assert_eq!(libz_version.contains("zlib-ng"), false);
+        if cfg!(feature = "zlib-ng-compat") {
+            assert_eq!(libz_version.contains("zlib-ng"), true);
+        } else {
+            assert_eq!(libz_version.contains("zlib-ng"), false);
+        }
     }
 }

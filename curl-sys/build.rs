d21f2d6bca0e4a6c65fbbd02f90e916c7ac69ad9
Eric Huss <eric@huss.org>
2023-05-31 15:07:23 -0700

    Update curl to 8.1.2

--- curl-sys/build.rs
+++ curl-sys/build.rs
@@ -100,7 +100,7 @@ fn main() {
             .replace("@LIBCURL_LIBS@", "")
             .replace("@SUPPORT_FEATURES@", "")
             .replace("@SUPPORT_PROTOCOLS@", "")
-            .replace("@CURLVERSION@", "8.1.0"),
+            .replace("@CURLVERSION@", "8.1.2"),
     )
     .unwrap();
 

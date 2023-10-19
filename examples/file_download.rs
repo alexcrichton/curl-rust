7cc456dd27d1259bc58bb519f9f525095edca48c
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-10-20 07:08:19 +0800

    add response as body example using Easy2

--- examples/file_download.rs
+++ examples/file_download.rs
@@ -1,12 +1,17 @@
 /// This is an example how to use Easy2 to download a file.
 /// Can able to resume a download and control download speed.
-use std::{fs::OpenOptions, io::Write, path::PathBuf};
+use std::{
+    fs::{self, OpenOptions},
+    io::Write,
+    path::PathBuf,
+};
 
 use curl::easy::{Easy2, Handler, WriteError};
 
+#[derive(Clone)]
 enum Collector {
     File(PathBuf),
-    // You can add what type of storage you want
+    Ram(Vec<u8>),
 }
 
 impl Handler for Collector {
@@ -29,14 +34,32 @@ impl Handler for Collector {
                 })?;
                 Ok(data.len())
             }
+            Collector::Ram(container) => {
+                container.extend_from_slice(data);
+                Ok(data.len())
+            }
         }
     }
 }
-fn main() {
-    let collector = Collector::File(PathBuf::from("<YOUR TARGET PATH>"));
+
+impl Collector {
+    fn get_response_body(&self) -> Option<Vec<u8>> {
+        match self {
+            Collector::File(_) => None,
+            Collector::Ram(container) => Some(container.clone()),
+        }
+    }
+}
+
+fn example_file_download() {
+    // File Download
+    let target_path = PathBuf::from("<SAVE DOWNLOADED FILE HERE>");
+    let collector = Collector::File(target_path.clone());
     let mut easy2 = Easy2::new(collector);
 
-    easy2.url("<YOUR DOWNLOAD LOCATION>").unwrap();
+    easy2
+        .url("<INPUT YOUR TARGET DOWNLOAD LOCATION HERE>")
+        .unwrap();
     easy2.get(true).unwrap();
     // Download the actual file from actual location
     easy2.follow_location(true).unwrap();
@@ -47,7 +70,38 @@ fn main() {
     easy2.perform().unwrap();
 
     let status_code = easy2.response_code().unwrap();
-    let content_type = easy2.content_type().unwrap();
-    eprintln!("Content Type: {:?}", content_type);
+    let response_body = easy2.get_ref().get_response_body().take();
+    let content_type = easy2.content_type();
+
     eprintln!("Status Code: {}", status_code);
+    eprintln!("content-type: {:?}", content_type);
+    eprintln!("Response Body: {:?}", response_body);
+
+    let file = fs::metadata(target_path.clone()).unwrap();
+    assert!(file.len() != 0);
+}
+
+fn example_response_as_body() {
+    // Get Response as Body
+    let collector = Collector::Ram(Vec::new());
+    let mut easy2 = Easy2::new(collector);
+
+    easy2
+        .url("<INPUT YOUR TARGET DOWNLOAD LOCATION HERE>")
+        .unwrap();
+    easy2.get(true).unwrap();
+    easy2.perform().unwrap();
+
+    let status_code = easy2.response_code().unwrap();
+    let response_body = easy2.get_ref().get_response_body().take();
+    let content_type = easy2.content_type();
+
+    eprintln!("Status Code: {}", status_code);
+    eprintln!("content-type: {:?}", content_type);
+    eprintln!("Response Body: {:?}", response_body);
+}
+
+fn main() {
+    example_file_download();
+    example_response_as_body();
 }

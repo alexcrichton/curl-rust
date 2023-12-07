2a580b4074cff900875c799ef74f4a46e492fa45
Qiu Chaofan <qcf@ecnelises.com>
2023-12-07 17:03:45 +0800

    Update to curl 8.5.0

--- curl-sys/build.rs
+++ curl-sys/build.rs
@@ -100,7 +100,7 @@ fn main() {
             .replace("@LIBCURL_LIBS@", "")
             .replace("@SUPPORT_FEATURES@", "")
             .replace("@SUPPORT_PROTOCOLS@", "")
-            .replace("@CURLVERSION@", "8.4.0"),
+            .replace("@CURLVERSION@", "8.5.0"),
     )
     .unwrap();
 

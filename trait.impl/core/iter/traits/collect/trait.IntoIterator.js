7eb1c438ac7824edb78eb40eeb90c91b8eb5432a
ci <ci>
2024-07-30 14:38:24 +0000

    init

---
+++ trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"curl":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"curl/easy/struct.List.html\" title=\"struct curl::easy::List\">List</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
